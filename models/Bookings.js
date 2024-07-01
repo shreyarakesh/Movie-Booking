b57bd166e461720fa85cd9be06581a333c33235b
shreyarakesh <127003176+shreyarakesh@users.noreply.github.com>
2024-07-01 19:10:02 +0300

    Update Bookings.js

--- models/Bookings.js
+++ models/Bookings.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const bookingSchema = new mongoose.Schema({
     movie: {
-        type: mongoose.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: "Movies",
         required: true
     },
@@ -15,14 +15,23 @@ const bookingSchema = new mongoose.Schema({
         required: true
     },
     user: {
-        type: mongoose.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: true
-    }
-    
-  });
+    },
+    showDate: {
+        type: Date,
+        required: true
+    },
+    showTime: {
+        type: String,
+        required: true
+    },
+});
 
+// Compound index to ensure uniqueness of combination of movie, showDate, showTime, seatNumber
+bookingSchema.index({ movie: 1, showDate: 1, showTime: 1, seatNumber: 1 }, { unique: true });
 
-  const Booking = mongoose.model('booking', bookingSchema);
-  
-  export default Booking;
+const Booking = mongoose.model('movbookings', bookingSchema);
+
+export default Booking;
