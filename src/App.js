1de2befc3e53134522814d270305d51f2d2e20cf
shreyarakesh <127003176+shreyarakesh@users.noreply.github.com>
2024-07-01 19:29:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { useEffect } from "react";
-import Header from "./components/Header";
 import { Route, Routes, Navigate } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import Movies from "./components/Movies/Movies";
-
-import Admin from "./components/Admin/Admin";
-import Auth from "./components/Auth/Auth";
 import { useDispatch, useSelector } from "react-redux";
 import { adminActions, userActions } from "./store";
-import Booking from "./components/Bookings/Bookings";
 
+import Header from "./components/Header";
+import HomePage from "./components/HomePage";
+import Movies from "./components/Movies/Movies";
+import Admin from "./components/Admin/Admin";
+import Auth from "./components/Auth/Auth";
+import Booking from "./components/Bookings/Bookings";
+import UserProfile from "./profile/UserProfile";
+import AddMovie from "./components/Movies/AddMovie";
+import AdminProfile from "./profile/AdminProfile";
+import MovieDetails from "./components/Movies/MovieDetails";
+import ErrorPage from "./components/ErrorPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   // Retrieve login states from the Redux store
@@ -36,12 +41,47 @@ function App() {
           {/* Public routes */}
           <Route path="/" element={<HomePage />} />
           <Route path="/movies" element={<Movies />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/booking/:id" element={<Booking />} />
-          
-          
-         
+          <Route path="/movies/:id" element={<MovieDetails />} />
+          {!isAdminLoggedIn && !isUserLoggedIn && (
+            <>
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/auth" element={<Auth />} />
+            </>
+          )}
+          <Route
+            path="/user"
+            element={
+              <ProtectedRoute condition={!isAdminLoggedIn && isUserLoggedIn}>
+                <UserProfile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/booking/:id"
+            element={
+              <ProtectedRoute condition={!isAdminLoggedIn && isUserLoggedIn}>
+                <Booking />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/add"
+            element={
+              <ProtectedRoute condition={isAdminLoggedIn && !isUserLoggedIn}>
+                <AddMovie />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin-user"
+            element={
+              <ProtectedRoute condition={isAdminLoggedIn && !isUserLoggedIn}>
+                <AdminProfile />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/error" element={<ErrorPage />} /> {/* Error page route */}
+          <Route path="*" element={<Navigate to="/error" />} /> {/* Catch-all route */}
         </Routes>
       </section>
     </div>
